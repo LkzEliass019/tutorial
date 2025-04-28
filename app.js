0cb8c6c289924e58ae2f239d8c65c33cd7ab7fd4
LkzEliass019 <lucas2201alcantara@gmail.com>
2025-04-28 14:35:34 -0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const session = require("express-session")
-const sqlite3 = require("sqlite3")
+const session = require("express-session");
+const sqlite3 = require("sqlite3");
 const app = express(); // Armazena as chamadas e propriedades da biblioteca EXPRESS
 
 const PORT = 8000;
@@ -10,23 +10,34 @@ app.use('/static', express.static(__dirname + '/static'));
 app.set('view engine', 'ejs');
 
 app.get ("/", (req, res) => {
-    console.log("GET /")
+    console.log("GET /");
     // res.send("Alô SESI Sumaré<br>Bem-vindos ao SENAI Sumaré.");
     //res.send("<img src='./static/image.jpg' width='30%'/>" );
-    res.render("index" );
+    res.render("./pages/index" );
 })
 
 app.get ("/sobre", (req, res) => {
-    console.log("GET /sobre")
-   res.render("sobre")
+    console.log("GET /sobre");
+   res.render("./pages/sobre");
 })
+
+app.get ("/cadastro", (req, res) => {
+    console.log("GET /cadastro");
+    res.render("./pages/cadastro");
+})
+
+app.get ("/login", (req, res) => {
+    console.log("GET /login");
+    res.render("./pages/login");
+
+})
+
 app.get ("/dashboard", (req, res) => {
-    console.log("GET /dashboard")
-    res.send("Você está na página DASHBOARD.");
+    console.log("GET /dashboard");
+    res.render("./pages/dashboard");
 })
 
 app.listen(PORT, () =>{
-    console.log(`Servidor sendo executado na porta ${PORT}`)
+    console.log(`Servidor sendo executado na porta ${PORT}`);
     console.log(__dirname + "\\static");
 });
-
